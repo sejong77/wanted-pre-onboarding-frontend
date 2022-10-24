1d41935b11b292b2b84b6dea1c19ac5cf570f865
sejong77 <77sejong@gmail.com>
2022-10-24 17:54:37 +0900

    폰트 전역 설정으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import './App.module.scss';
+import './fonts/Font.scss';
+
 import AuthPage from './pages/Auth/AuthPage';
 
 const App = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import './fonts/Font.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
