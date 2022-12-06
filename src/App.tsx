7f552b86ba1f8988840b4c19e9bf04c3db2427b7
sejong77 <77sejong@gmail.com>
2022-12-06 10:03:07 +0900

    투두리스트 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.module.scss';
 import './fonts/Font.scss';
 
 import AuthPage from './pages/Auth/AuthPage';
+import TodoPage from './pages/Todo/TodoPage';
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<AuthPage />} />
+				<Route path="/todo" element={<TodoPage />} />
 			</Routes>
 		</Router>
 	);
