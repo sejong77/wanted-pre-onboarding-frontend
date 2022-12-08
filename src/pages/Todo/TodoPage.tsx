ac71742259a49f29e823c563b26cd3101f1cffae
sejong77 <77sejong@gmail.com>
2022-12-08 16:16:48 +0900

    투두 리스트 삭제 버튼 추가
    1. 체크박스 and 삭제 버튼에 마우스 올릴 경우 마우스 커서 포인터로 변경
    
    2. 투두리스트 항목에 마우스 커서 올릴 경우 해당 항목에 삭제 버튼 표시

--- src/pages/Todo/TodoPage.tsx
+++ src/pages/Todo/TodoPage.tsx
@@ -3,6 +3,7 @@ import styles from './TodoPage.module.scss';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 
 import { useEffect, useState } from 'react';
 
@@ -11,6 +12,7 @@ import { TodoListProps } from '../../interfaces/interface';
 
 const TodoPage = () => {
 	const [todos, setTodos] = useState<TodoListProps[]>([]);
+	const [item, setItem] = useState<number>(0);
 
 	const getTodos = () => {
 		callGetAPI('/todos').then((res) => {
@@ -30,10 +32,26 @@ const TodoPage = () => {
 
 				<ul className={styles.list}>
 					{todos.map((todo) => (
-						<li key={todo.id} className={styles.item}>
-							{todo.isCompleted ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon />}
+						<li
+							key={todo.id}
+							className={styles.item}
+							onMouseEnter={() => setItem(todo.id)}
+							onMouseLeave={() => setItem(0)}
+						>
+							<div className={styles.center}>
+								{todo.isCompleted ? (
+									<CheckBoxIcon className={styles.icon} />
+								) : (
+									<CheckBoxOutlineBlankIcon className={styles.icon} />
+								)}
+								{todo.todo}
+							</div>
 
-							<span>{todo.todo}</span>
+							<div className={styles.center}>
+								{item === todo.id && (
+									<DeleteRoundedIcon className={`${styles.delete} ${styles.icon}`} />
+								)}
+							</div>
 						</li>
 					))}
 				</ul>
