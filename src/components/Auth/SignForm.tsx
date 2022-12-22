f96861db0b8eaa6c886c4dfaf855641c36764e91
sejong77 <77sejong@gmail.com>
2022-12-22 13:50:12 +0900

    refactor: 하드코딩 했던 Alert 문구를 상수로 변경

--- src/components/Auth/SignForm.tsx
+++ src/components/Auth/SignForm.tsx
@@ -1,12 +1,13 @@
 import * as S from '@pages/Auth/AuthStyle';
 
-import { callPostAPI } from '@api/api';
-import { setAccessToken } from '@lib/AuthLocalStorage';
-import Mode from './Mode';
-
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useInput from '@hooks/useInput';
+
+import { callPostAPI } from '@api/api';
+import { setAccessToken } from '@lib/AuthLocalStorage';
+import { SIGN_IN, SIGN_UP } from '@constants/string/Auth';
+import Mode from './Mode';
 
 const SignForm = () => {
 	const navigate = useNavigate();
@@ -31,11 +32,11 @@ const SignForm = () => {
 			callPostAPI('auth/signup', { email: values.email, password: values.password }).then(
 				(res) => {
 					console.log('회원가입 데이터: ', res);
-					alert('회원가입에 성공했습니다.');
+					alert(SIGN_UP.SUCCESS);
 					setTitle('로그인');
 				},
 				(err) => {
-					alert('회원가입에 실패했습니다.');
+					alert(SIGN_UP.FAIL);
 					throw new Error(err);
 				}
 			);
@@ -43,11 +44,11 @@ const SignForm = () => {
 			callPostAPI('auth/signin', { email: values.email, password: values.password }).then(
 				(res) => {
 					setAccessToken(res.data.access_token);
-					alert('로그인에 성공했습니다.');
+					alert(SIGN_IN.SUCCESS);
 					navigate('/todo');
 				},
 				(err) => {
-					alert('로그인에 실패했습니다.');
+					alert(SIGN_IN.FAIL);
 					throw new Error(err);
 				}
 			);
