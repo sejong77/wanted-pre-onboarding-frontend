51a87b690562c0e8beb1cb8b7e932cfa699340e2
sejong77 <77sejong@gmail.com>
2022-12-22 15:38:12 +0900

    design: 로그인 / 회원가입 CSS 수정
    - 배경색 변경
    - 경계선(border) 제거
    - 글꼴 크기 및 색상 변경
    - 버튼 색상 변경
    - padding, margin 변경

--- src/pages/Auth/AuthStyle.ts
+++ src/pages/Auth/AuthStyle.ts
@@ -7,38 +7,42 @@ export const AuthContainer = styled.div`
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
-	background-color: #d5e9ee;
+	background-color: #f5f5f5;
 `;
 
 export const AuthWrapper = styled.div`
 	max-width: 350px;
+	max-height: 400px;
 	width: 100%;
-	border: 1px solid #9c9c9c;
+	height: 100%;
+	border-radius: 10px;
 	text-align: center;
-	padding: 20px 0;
-	background-color: #ffffff;
+	padding: 20px;
 `;
 
 export const Title = styled.p`
-	font-size: 30px;
+	font-size: 40px;
 	font-weight: bold;
-`;
-
-export const Form = styled.form`
-	padding: 10px 40px 30px 40px;
+	color: #3080bf;
+	text-align: left;
+	margin-bottom: 12px;
 `;
 
 export const Input = styled.input`
-	padding: 7px;
+	padding: 12px 7px;
 	width: 100%;
 	background-color: #fafafa;
 	border: 1px solid #9c9c9c;
-	margin-top: 28px;
 	box-sizing: border-box;
+	font-size: 16px;
 
 	&:focus {
 		outline: 1px solid #4dbeff;
 		border: 1px solid #4dbeff;
+	}
+
+	&:last-of-type {
+		margin-top: 28px;
 	}
 `;
 
@@ -54,10 +58,11 @@ export const Button = styled.button<InputProps>`
 	border-radius: 5px;
 	width: 100%;
 	padding: 12px;
-	margin-top: 28px;
+	margin: 28px 0;
+	font-size: 16px;
 
 	background-color: ${(props) =>
-		props.isEmailValid && props.isPasswordValid ? '#4dbeff' : '#c3c3c3'};
+		props.isEmailValid && props.isPasswordValid ? '#3080bf' : '#c3c3c3'};
 
 	color: ${(props) => (props.isEmailValid && props.isPasswordValid ? '#ffffff' : '#777')};
 
@@ -67,7 +72,7 @@ export const Button = styled.button<InputProps>`
 `;
 
 export const AuthMode = styled.p`
-	color: #666666;
+	color: #3080bf;
 
 	&:hover {
 		cursor: pointer;
