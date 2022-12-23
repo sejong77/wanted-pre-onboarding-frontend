6d0342643381d35cbd48ea19b594b5a9647dcdd7
sejong77 <77sejong@gmail.com>
2022-12-23 15:09:31 +0900

    design: 전역 스타일링 적용방식 변경
    기존 App.module.scss파일에 전역 스타일링 코드를 작성하여 구현하는 방식에서
    styled-components의 createGlobalStyle을 사용하여 전역 스타일링을 구현하는
    방식으로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './fonts/Font.scss';
+
 import ModalProvider from '@contexts/Modal/ModalProvider';
+import GlobalStyle from '@styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<ModalProvider>
+		<GlobalStyle />
 		<App />
 	</ModalProvider>
 );
