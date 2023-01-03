e0e17555177c1e7651f1bf4540aee77af199fba5
sejong77 <77sejong@gmail.com>
2023-01-03 17:41:04 +0900

    feat: 투두리스트 생성 기능 추가
    토글 버튼 클릭 -> 입력창 렌더링 -> 입력창에 투두리스트에 추가할 항목 입력 -> Enter 클릭
    
    투두 리스트 생성

--- src/components/Todo/TodoCreate/TodoCreate.tsx
+++ src/components/Todo/TodoCreate/TodoCreate.tsx
@@ -1,17 +1,41 @@
 import * as T from './style';
 
-import { useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
+
+import { callPostAPI } from '@api/api';
+import { dispatchContext } from '@contexts/Todo/TodoContext';
+import { useEffect } from 'react';
 
 const TodoCreate = () => {
 	const [toggle, setToggle] = useState<boolean>(false);
 	const [content, setContent] = useState<string>('');
 
 	const inputRef = useRef<HTMLInputElement>(null);
+	const dispatch = useContext(dispatchContext);
 
 	const onToggle = () => {
 		setToggle((prev) => !prev);
 		setContent('');
 	};
+
+	const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			inputRef.current.value = '';
+			createTodos();
+		}
+	};
+
+	const createTodos = () => {
+		callPostAPI('/todos', { todo: content }).then((res) => {
+			dispatch({ type: 'ADD', todo: res.data });
+		});
+	};
+
+	useEffect(() => {
+		if (toggle) {
+			inputRef.current.focus();
+		}
+	}, [toggle]);
 
 	return (
 		<>
@@ -22,6 +46,7 @@ const TodoCreate = () => {
 						placeholder="할 일을 입력 후, Enter를 누르세요."
 						value={content}
 						onChange={(e) => setContent(e.target.value)}
+						onKeyDown={onKeyDown}
 					/>
 				</T.InputWrapper>
 			)}
